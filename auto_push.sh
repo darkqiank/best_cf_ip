37a3fde3e4ad789ece4e75d615bdcf5f61857e36
darkqiank <darkqiank@foxmail.com>
2024-06-26 09:15:32 +0000

    Automated commit

--- auto_push.sh
+++ auto_push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /home/ubuntu/cf
+./CloudflareST -cfcolo OKA,NRT,FUK,KIX -tl 300 -p 60 -dd
 cd /home/ubuntu/best_cf_ip
 /home/ubuntu/miniconda3/bin/python update.py /home/ubuntu/cf/result.csv 60
 git add .
