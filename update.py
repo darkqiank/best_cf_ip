88498984d5e97a49862ad2024ca57d77bf12d316
darkqiank <darkqiank@foxmail.com>
2024-06-26 09:36:58 +0000

    Automated commit

--- update.py
+++ update.py
@@ -23,6 +23,28 @@ def main():
     with open('addressesapi.txt', 'w') as f:
         for address in ipports:
             f.write(address + '\n')
+    print(f"addressesapi.txt写入{n}个节点")
+    
+    random_rows = df.sample(n)
+    ipports = random_rows['IP 地址'].apply(turn_data)
+    
+    # 写入文件
+    with open('addressesapi_1.txt', 'w') as f:
+        for address in ipports:
+            f.write(address + '\n')
+    print(f"addressesapi_1.txt写入{n}个节点")
+
+
+    random_rows = df.sample(n)
+    ipports = random_rows['IP 地址'].apply(turn_data)
+    
+    # 写入文件
+    with open('addressesapi_2.txt', 'w') as f:
+        for address in ipports:
+            f.write(address + '\n')
+    print(f"addressesapi_2.txt写入{n}个节点")
+
+
 
 if __name__ == "__main__":
     main()
