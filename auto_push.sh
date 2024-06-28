e29b571bfcd05df255816a29fd7acf8441c340a1
darkqiank <darkqiank@foxmail.com>
2024-06-28 05:54:01 +0000

    Automated commit

--- auto_push.sh
+++ auto_push.sh
@@ -2,7 +2,7 @@
 cd /home/ubuntu/cf
 ./CloudflareST -cfcolo OKA,NRT,FUK,KIX -tl 300 -p 60 -dd
 cd /home/ubuntu/best_cf_ip
-/home/ubuntu/miniconda3/bin/python update.py /home/ubuntu/cf/result.csv 60
+/home/ubuntu/miniconda3/bin/python update.py /home/ubuntu/cf/result.csv 20
 git add .
 git commit -m "Automated commit"
 git push origin master
